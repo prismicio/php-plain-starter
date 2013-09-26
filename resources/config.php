f91f5b06c17b00e9b18a8416cf9abd96d743dfa2
Sebastien Renault <sre@zenexity.com>
2013-09-26 11:04:53 +0200

    config + vendors

--- resources/config.php
+++ resources/config.php
@@ -1,12 +1,21 @@
 <?php
 
 $config = array(
+    "prismic" => array(
+        "api" => "https://prismic.io",
+        "token" => "xxx",
+        "clientId" => "xxx",
+        "clientSecret" => "xxx"
+    )
 );
 
-defined("LIBRARY_PATH") or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+defined("LIBRARIES_PATH") or define("LIBRARIES_PATH", realpath(dirname(__FILE__) . '/libraries'));
+
+defined("VENDORS_PATH") or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/libraries/vendors'));
 
 defined("TEMPLATES_PATH") or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
 
 ini_set("error_reporting", "true");
 error_reporting(E_ALL|E_STRCT);
+
 ?>
