af205bc9051f952aaba6bc97d739e963ae8fea22
Rudy Rigot <rudy.rigot@gmail.com>
2014-03-10 15:47:01 -0700

    Integrating Bootstrap3

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,11 +1,14 @@
-    <footer>
-      <hr/>
-      <?php
-          $hasPrivilegedAccess = $ctx->hasPrivilegedAccess();
-          if (!$hasPrivilegedAccess) {
-             echo '<a href="'. Routes::signin() . '">Sign in to preview changes</a>';
-          }
-      ?>
-    </footer>
+      <footer>
+        <hr/>
+        <?php
+            $hasPrivilegedAccess = $ctx->hasPrivilegedAccess();
+            if (!$hasPrivilegedAccess) {
+               echo '<a href="'. Routes::signin() . '">Sign in to preview changes</a>';
+            }
+        ?>
+      </footer>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="en">
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta content="text/html; Charset=UTF-8" http-equiv="Content-Type" />
     <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
   </head>
   <body>
-     <header>
-       <?php require_once(TEMPLATES_PATH . "/toolbar.php"); ?>
-       <a href="#">
-         <h1>Your prismic.io project</h1>
-       </a>
-       <hr/>
-     </header>
+    <?php require_once(TEMPLATES_PATH . "/navigation.php"); ?>
+    <div class="container">
+       <header>
+         <?php require_once(TEMPLATES_PATH . "/toolbar.php"); ?>
+       </header>
